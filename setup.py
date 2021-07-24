54957bc0a748567311178f979a5c85471b813374
Jules Leguy <jules.leguy@etud.univ-angers.fr>
2021-07-24 10:40:01 +0200

    update code to use relative references to modules and to use the EvoMol installation package
    Adding setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='BBOMol',
+version='0.1',
+description='Surrogate-based black-box optimization method for molecular properties',
+url='https://github.com/jules-leguy/BBOMol',
+author='Jules Leguy',
+author_email='jules.leguy@univ-angers.fr',
+license='LGPL',
+packages=find_packages(),
+zip_safe=False)
